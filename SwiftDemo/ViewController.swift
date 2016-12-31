bd21c7a10177d0c4a6f959bb22fb021d8c11ef48
Casa Taloyum <casatwy@msn.com>
2016-12-31 12:31:27 +0800

    Initial Commit

---
+++ SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by casa on 2016/12/31.
+//  Copyright © 2016年 casa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
